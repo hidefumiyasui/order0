796850751f9f742d91ce9fcb8e437d883756c72f
hidefumiyasui <skyblue6000@icloud.com>
2020-09-19 23:05:57 +0900

    商品一覧機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_080756) do
+ActiveRecord::Schema.define(version: 2020_09_19_081520) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,13 +21,15 @@ ActiveRecord::Schema.define(version: 2020_09_18_080756) do
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
-  create_table "credit_cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "explanation"
+    t.integer "price"
+    t.string "image"
+    t.bigint "genre_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_credit_cards_on_user_id"
+    t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -47,5 +49,4 @@ ActiveRecord::Schema.define(version: 2020_09_18_080756) do
   end
 
   add_foreign_key "cards", "users"
-  add_foreign_key "credit_cards", "users"
 end
